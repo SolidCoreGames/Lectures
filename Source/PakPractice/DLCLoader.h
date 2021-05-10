b0a6902f6fb160c3b432515fdaea5c96998a2757
Artem Avetisov <mrdoooser@gmail.com>
2021-05-10 19:50:21 +0300

    PAK_Practice project init push

---
+++ Source/PakPractice/DLCLoader.h
@@ -0,0 +1,71 @@
+// Copyright (c) 2019-2020 Mastersolution AG, Germany, all rights reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "MapInfoAsset.h"
+#include "UObject/NoExportTypes.h"
+
+#include "Runtime/PakFile/Public/IPlatformFilePak.h"
+#include "DLCLoader.generated.h"
+
+
+class PAKPRACTICE_API FDLCLoaderFileVisitor : public IPlatformFile::FDirectoryVisitor
+{
+public:
+	virtual bool Visit(const TCHAR* FilenameOrDirectory, bool bIsDirectory) override
+	{
+		if (!bIsDirectory)
+		{
+			Files.Add(FilenameOrDirectory);
+		}
+		return true;
+	}
+
+	TArray<FString> Files;
+};
+/**
+ * 
+ */
+UCLASS()
+class PAKPRACTICE_API UDLCLoader : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+
+	bool ReadStudiosInfo();
+
+	TArray<FMapInfo> GetMapsInfo();
+    
+    void Clear();
+
+private:
+
+	TArray<FString> GetClassesToLoad();
+
+	UPROPERTY()
+	TArray<FMapInfo> _mapsInfo;
+
+	TArray<FString> LoadDLC();
+
+	// DLC methods
+	bool MountDLC(const FString & PakFilename);
+	bool UnmountDLC(const FString& PakFilename);
+	int32 GetDLCOrder(const FString & PakFilePath);
+
+	void RegisterMountPoint(const FString & RootPath, const FString & ContentPath);
+
+	TArray<FString> GetFilesInDLC(const FString & Directory);
+	bool ReadPakFile(FString PakFileName);
+	UClass * LoadClassFromDLC(const FString & Filename);
+
+	FPakPlatformFile *GetDLCFile();
+	FPakPlatformFile *DLCFile = nullptr;
+
+#if UE_BUILD_SHIPPING == 0
+	IPlatformFile *OriginalPlatformFile = nullptr;
+#endif
+    
+};
---
+++ Source/PakPractice/PakPracticeGameInstance.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "DLCLoader.h"
+#include "Engine/GameInstance.h"
+#include "PakPracticeGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PAKPRACTICE_API UPakPracticeGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY()
+	UDLCLoader * _DLCLoader;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	TArray<FMapInfo> GetMapsInfo();
+	
+protected:
+	void Init() override;
+	
+};
